6476afa7acb58ce61a256c25454084c58f93be39
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-08-14 15:15:46 +0300

    Create UserSerializer and implement base ViewSet for users list

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,13 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from user.views import UserViewSet
+
+router = DefaultRouter()
+router.register("users-list", UserViewSet)
 
 urlpatterns = [
-    
+    path("", include(router.urls))
 ]
 
 app_name = "user"
