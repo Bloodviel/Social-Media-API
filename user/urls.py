77886409db4d1eb016a3e13899466edf5aa32838
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-08-14 15:48:11 +0300

    Implement Token authentication

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from user.views import CreateUserView, UserViewSet
+from user.views import CreateUserView, UserViewSet, CreateTokenView
 
 router = DefaultRouter()
 router.register("users-list", UserViewSet)
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
+    path("token/", CreateTokenView.as_view(), name="token"),
     path("", include(router.urls)),
 ]
 
