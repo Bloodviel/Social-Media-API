4d384ee9916e2eeb537006ae65760a60b3ff0439
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-08-15 13:58:07 +0300

    Make opportunity to manage user's profile

--- user/views.py
+++ user/views.py
@@ -1,13 +1,11 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets, generics, status
-from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.settings import api_settings
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from user.serializers import UserSerializer, AuthTokenSerializer
+from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -17,7 +15,14 @@ class CreateUserView(generics.CreateAPIView):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
 
 
 class LogoutView(APIView):
